82c3c290409974519c365e73ff49c9d35761d3bb
Suraj Sarkar <surajsarkar1002@gmail.com>
2023-01-13 02:15:19 +0530

    add component repo-info

---
+++ src/app/repo-info/repo-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-info',
+  templateUrl: './repo-info.component.html',
+  styleUrls: ['./repo-info.component.css']
+})
+export class RepoInfoComponent {
+
+}
