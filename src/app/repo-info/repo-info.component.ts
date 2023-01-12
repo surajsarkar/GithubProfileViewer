307e881465dbd19630cf3e9af224ae19aa199eb4
Suraj Sarkar <surajsarkar1002@gmail.com>
2023-01-13 03:20:22 +0530

    add inuts to repo info

--- src/app/repo-info/repo-info.component.ts
+++ src/app/repo-info/repo-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-repo-info',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./repo-info.component.css']
 })
 export class RepoInfoComponent {
+
+  @Input() repoName: string = '';
+  @Input() repoDes: string = '';
+  @Input() topics: Array<string> = ["angular", "vercel", "tutorial", "skillup"];
 
 }
