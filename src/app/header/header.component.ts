6bfb669bc6622c3c860779200ca805f4dc91a275
Suraj Sarkar <surajsarkar1002@gmail.com>
2023-01-13 01:11:57 +0530

    add decorators for input

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Input() name: string = "";
+  @Input() bio: string = "";
+  @Input() location: string = "";
+  @Input() github_user_name: string = "";
+  @Input() twitter: string = "surajsarkar";
 
 }
