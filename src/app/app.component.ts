26446e67d2775c540c10ae6597850239082b4e80
Suraj Sarkar <surajsarkar1002@gmail.com>
2023-01-13 19:52:30 +0530

    add search function add api fetching functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProfileViewer';
+  private apiBaseUrl = `http://localhost:5000/api/user?name=`;
   repoTopic: Array<string> = ["skillup", "trail", "building", "samurai"];
+
+  userDetails: any = {};
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type" : "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:5000/api/user"
+    })
+  }
+
+  constructor(private httpClint: HttpClient){}
+
+  username: string = "";
+  
+  search(){
+    console.log(`Username: ${this.username}`);
+    const url =  `${this.apiBaseUrl}${this.username}`;
+    console.log(`URL: ${url}`);
+    
+    this.httpClint.get(url, this.httpOptions).subscribe((content) => {
+      this.userDetails = content;
+      console.log(this.userDetails);
+    });
+  }
+
+
 }
